782a44da2f7a7616f1419285ddb904c5ee7329d8
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-29 12:01:41 +0000

    Add files via upload
    The synth sound template that Chris made for me

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,50 @@
+var polySynth;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(500, 500);
     canvas.parent("p5container");
+
+    polySynth = new p5.PolySynth();
 }
+
+function playSynth1(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('G2', vel, 0, dur);
+    polySynth.play('C3', vel, time += 1/3, dur);
+    polySynth.play('G3', vel, time += 1/3, dur);
+
+}
+
+function playSynth2(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('G3', vel, 0, dur);
+    polySynth.play('C3', vel, time += 1/3, dur);
+    polySynth.play('G2', vel, time += 1/3, dur);
+
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW){
+        playSynth1();
+        fill(255, 255, 0);
+    } else if(keyCode === RIGHT_ARROW){
+        playSynth2();
+        fill(255, 0, 255);
+    }
+}
+
 
 function draw() {
     background(220);
-
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(width/2, height/2, 200);
 }
